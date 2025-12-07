940b85ab04c6414836e9362a8a23945d4d1a825a
Hasan Furkan Fidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2025-12-07 17:33:53 +0300

    GetById

--- src/services/catelog/MicroserviceProject.Catalog.Api/Features/Categories/CategoryEndpointExt.cs
+++ src/services/catelog/MicroserviceProject.Catalog.Api/Features/Categories/CategoryEndpointExt.cs
@@ -1,5 +1,6 @@
 ﻿using MicroserviceProject.Catalog.Api.Features.Categories.Create;
 using MicroserviceProject.Catalog.Api.Features.Categories.GetAll;
+using MicroserviceProject.Catalog.Api.Features.Categories.GetById;
 
 namespace MicroserviceProject.Catalog.Api.Features.Categories
 {
@@ -7,7 +8,7 @@ namespace MicroserviceProject.Catalog.Api.Features.Categories
     {
         public static void AddCategoryGroupEndpointExt(this WebApplication app)
         {
-            app.MapGroup("api/categories").CreateCategoryGroupItemEndpoint().GetAllCategoryGroupItemEndpoint();
+            app.MapGroup("api/categories").CreateCategoryGroupItemEndpoint().GetAllCategoryGroupItemEndpoint().GetByIdCategoryGroupItemEndpoint();
         }
     }
 }
---
+++ src/services/catelog/MicroserviceProject.Catalog.Api/Features/Categories/GetById/GetCategoryByIdEndpoint.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using MediatR;
+using MicroserviceProject.Catalog.Api.Features.Categories.Dtos;
+using MicroserviceProject.Catalog.Api.Repositories;
+using MicroserviceProject.Shared;
+using MicroserviceProject.Shared.Extensions;
+
+namespace MicroserviceProject.Catalog.Api.Features.Categories.GetById
+{
+    public record GetCategoryByIdQuery(Guid Id) : IRequest<ServiceResult<CategoryDto>>;
+    public class GetCategoryByIdQueryHandler(AppDbContext context,IMapper mapper) : IRequestHandler<GetCategoryByIdQuery, ServiceResult<CategoryDto>>
+    {
+        public async Task<ServiceResult<CategoryDto>> Handle(GetCategoryByIdQuery request, CancellationToken cancellationToken)
+        {
+            var result = await context.Categories.FindAsync(request.Id, cancellationToken);
+            if (result == null)
+                return ServiceResult<CategoryDto>.Error("Not Found", $"Category with Id {request.Id} was not found.", System.Net.HttpStatusCode.NotFound);
+
+            var dto = mapper.Map<CategoryDto>(result);
+
+            return ServiceResult<CategoryDto>.SuccessAsOk(dto);
+        }
+    }
+    public static class GetCategoryByIdEndpoint
+    {
+        public static RouteGroupBuilder GetByIdCategoryGroupItemEndpoint(this RouteGroupBuilder group)
+        {
+            group.MapGet("/{id:guid}", async (Guid id, IMediator mediator) =>
+            {
+                var result = await mediator.Send(new GetCategoryByIdQuery(id));
+
+                return result.ToGenericResult();
+            });
+
+            return group;
+        }
+    }
+}
