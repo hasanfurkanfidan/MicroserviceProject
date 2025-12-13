7aad942bf70d30839c520b39be5805e548fe314e
Hasan Furkan Fidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2025-12-13 20:29:41 +0300

    updates

--- src/services/catelog/MicroserviceProject.Catalog.Api/Features/Courses/CourseEndpointExt.cs
+++ src/services/catelog/MicroserviceProject.Catalog.Api/Features/Courses/CourseEndpointExt.cs
@@ -1,6 +1,7 @@
 ﻿using MicroserviceProject.Catalog.Api.Features.Courses.Create;
 using MicroserviceProject.Catalog.Api.Features.Courses.Delete;
 using MicroserviceProject.Catalog.Api.Features.Courses.GetAll;
+using MicroserviceProject.Catalog.Api.Features.Courses.GetAllByUserId;
 using MicroserviceProject.Catalog.Api.Features.Courses.GetById;
 using MicroserviceProject.Catalog.Api.Features.Courses.Update;
 
@@ -10,7 +11,7 @@ namespace MicroserviceProject.Catalog.Api.Features.Courses
     {
         public static void AddCourseGroupEndpointExt(this WebApplication app)
         {
-            app.MapGroup("api/courses").WithTags("Courses").CreateCourseGroupItemEndpoint().GetAllCourseGroupItemEndpoint().GetByIdCourseGroupItemEndpoint().UpdateCourseGroupItemEndpoint().DeleteCourseGroupItemEndpoint();
+            app.MapGroup("api/courses").WithTags("Courses").CreateCourseGroupItemEndpoint().GetAllCourseGroupItemEndpoint().GetByIdCourseGroupItemEndpoint().UpdateCourseGroupItemEndpoint().DeleteCourseGroupItemEndpoint().GetAllCoursesByUserIdGroupItemEndpoint();
         }
     }
 }
---
+++ src/services/catelog/MicroserviceProject.Catalog.Api/Features/Courses/GetAllByUserId/GetAllCoursesByUserIdEndpoint.cs
@@ -0,0 +1,35 @@
+﻿using MicroserviceProject.Catalog.Api.Features.Courses.Dtos;
+using MicroserviceProject.Catalog.Api.Repositories;
+
+namespace MicroserviceProject.Catalog.Api.Features.Courses.GetAllByUserId
+{
+
+    public record GetAllCoursesByUserIdQuery(Guid UserId) : IRequestByServiceResult<List<CourseDto>>;
+    public class GetAllCoursesByUserIdQueryHandler(AppDbContext context, IMapper mapper) : IRequestHandler<GetAllCoursesByUserIdQuery, ServiceResult<List<CourseDto>>>
+    {
+        public async Task<ServiceResult<List<CourseDto>>> Handle(GetAllCoursesByUserIdQuery request, CancellationToken cancellationToken)
+        {
+            var courses = await context.Courses
+                .Where(c => c.UserId == request.UserId)
+                .ToListAsync(cancellationToken);
+
+            var mappedCourses = mapper.Map<List<CourseDto>>(courses);
+            return ServiceResult<List<CourseDto>>.SuccessAsOk(mappedCourses);
+        }
+    }
+
+    public static class GetAllCoursesByUserIdEndpoint
+    {
+        public static RouteGroupBuilder GetAllCoursesByUserIdGroupItemEndpoint(this RouteGroupBuilder group)
+        {
+            group.MapGet("user/{userId:guid}", async (Guid userId, IMediator mediator) =>
+            {
+                var result = await mediator.Send(new GetAllCoursesByUserIdQuery(userId));
+
+                return result.ToGenericResult();
+            }).WithName("GetAllCoursesByUserId");
+
+            return group;
+        }
+    }
+}
