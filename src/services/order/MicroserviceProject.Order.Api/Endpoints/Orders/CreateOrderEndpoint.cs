c637e928d77f1e3e362d1a52f3905a878cc09518
Hasan Furkan Fidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2025-12-22 21:25:08 +0300

    updates

---
+++ src/services/order/MicroserviceProject.Order.Api/Endpoints/Orders/CreateOrderEndpoint.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using MicroserviceProject.Order.Application.Features.Orders.Create;
+using MicroserviceProject.Shared.Filters;
+using MicroserviceProject.Shared.Extensions;
+namespace MicroserviceProject.Order.Api.Endpoints.Orders
+{
+    public static class CreateOrderEndpoint
+    {
+        public static RouteGroupBuilder CreateOrderGroupItemEndpoint(this RouteGroupBuilder group)
+        {
+            group.MapPost("/", async (CreateOrderCommand Command, IMediator mediator) =>
+            {
+                var result = await mediator.Send(Command);
+                return result.ToGenericResult();
+            }).WithName("CreateOrder").AddEndpointFilter<ValidationFilter<CreateOrderCommand>>();
+
+            return group;
+        }
+    }
+}
