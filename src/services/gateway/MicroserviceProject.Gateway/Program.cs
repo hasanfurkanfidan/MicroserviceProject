ef87eb1deb4e4a07be3e2b3a29b98d1d0f7a0c29
Hasan Furkan Fidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2026-01-04 14:11:36 +0300

    Updates

--- src/services/gateway/MicroserviceProject.Gateway/Program.cs
+++ src/services/gateway/MicroserviceProject.Gateway/Program.cs
@@ -1,9 +1,14 @@
+using MicroserviceProject.Shared.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
-
+builder.Services.AddAuthenticationAndAuthorizationExt(builder.Configuration);
 var app = builder.Build();
 
 app.MapReverseProxy();
 app.MapGet("/", () => "Yarp Gateway");
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
